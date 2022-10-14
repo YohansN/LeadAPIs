dd7ce5d1d1dd62114e1c182f72c5a65d6d9bfb5b
Yohans Nascimento <yohans.dev@gmail.com>
2022-10-14 15:40:18 -0300

    Adicionando conexão banco de dados

---
+++ Inventory/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-BKEG8HP;Initial Catalog=Inventory;Integrated Security=True");
+        }
+    }
+}
